c67aeb7eba2cbc920d9f33794592bc6bbbd344bd
XenonChan <chanachaisrifung@gmail.com>
2026-01-20 15:26:37 +0700

    Araiwaa

---
+++ compro2/src/class/BookStore.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+class Book {
+    public String isbn;
+    public String title;
+    public String type;
+    public double price;
+
+    public Book(String isbn, String title, String type, double price) {
+        this.isbn = isbn;
+        this.title = title;
+        this.type = type;
+        this.price = price;
+    }
+
+}
+public class BookStore {
+
+    ArrayList<Book> books = new ArrayList<Book>();
+
+    public void addBook(String isbn, String title, String type, double price) {
+        books.add(new Book(isbn, title, type, price));
+    }
+}
